29f5c57ca7ca200dcffb28e1430c4dffd9c38c14
droidovr <droidovr@gmail.com>
2023-09-19 21:07:22 +0300

    Lesson 1

---
+++ Assets/Core/Camera/Scripts/CameraFollower.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Platformer.Core.Camera
+{
+    public class CameraFollower : MonoBehaviour
+    {
+        [SerializeField] private Transform Player; // Player Object
+        [SerializeField] private float _speed = 0.01f; // Speed of camera (Lerp)
+
+        private Transform _camera; // Camera Object
+
+        private void Awake()
+        {
+            _camera = GetComponent<Transform>();
+        }
+
+        private void Update()
+        {
+            var cameraPosition = _camera.position; // Get position of Camera
+            cameraPosition.x = Mathf.Lerp(cameraPosition.x, Player.position.x, _speed); // Edit position
+
+            _camera.position = cameraPosition; // Update Camera position
+        }
+    }
+}
---
+++ Assets/Core/Player/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+namespace Platformer.Core.Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] private float _forceUp;
+        [SerializeField] private float _forceLeftRight;
+
+        private Rigidbody2D _player;
+
+        private void Awake()
+        {
+            _player = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            // Check keyboard keys
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                AddForceToPlayer(new Vector2(0, _forceUp)); // Move to up
+            }
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                AddForceToPlayer(new Vector2(-_forceLeftRight, 0)); // Move to left
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                AddForceToPlayer(new Vector2(_forceLeftRight, 0)); // Move to right
+            }
+        }
+
+        private void AddForceToPlayer(Vector2 force)
+        {
+            _player.AddForce(force); // Add force to player
+        }
+    }
+}
